ccf2989b12c5c90b8ba0d36fd0ef73dd3b969583
andreygranenko <131700684+andreygranenko@users.noreply.github.com>
2024-06-24 23:38:19 +0300

    added noteikumu page

---
+++ src/app/noteikumi/page.jsx
@@ -0,0 +1,9 @@
+const NoteikumuPage = () => {
+  return (
+    <div className={'container mt-5 '}>
+      <h2 className={'text-center text-2xl font-bold'}> Ceļu satiksmes noteikumi </h2>
+    </div>
+  );
+}
+
+export default NoteikumuPage;
