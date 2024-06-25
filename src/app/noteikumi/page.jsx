d8a35ff90c637e7d91be7c1971c2d4d3fbee151b
andreygranenko <131700684+andreygranenko@users.noreply.github.com>
2024-06-25 22:05:34 +0300

    updated noteikumu page

--- src/app/noteikumi/page.jsx
+++ src/app/noteikumi/page.jsx
@@ -1,7 +1,38 @@
+import Image from "next/image";
+
 const NoteikumuPage = () => {
   return (
-    <div className={'container mt-5 '}>
+    <div className={'container mx-auto mt-5 px-8 xl:px-24 '}>
       <h2 className={'text-center text-2xl font-bold'}> Ceļu satiksmes noteikumi </h2>
+      <h3 className={'text-xl font-bold mt-8'}>Satura rādītājs</h3>
+      <div className={'flex items-center lg:justify-between mt-8 gap-x-32 gap-y-10 flex-wrap'}>
+        <div className={'flex flex-col w-full xl:w-3/12  min-h-64 gap-3 relative'}>
+          <Image className={'rounded-md w-full'} src={'https://m-cdn.phonearena.com/images/article/130037-wide-two_1200/How-to-make-your-phone-louder.webp?1612969547'} alt={'chill'} width={'408'} height={'169'}/>
+          <h4 className={'text-left font-bold text-lg'}>1. Vispārīgie jautājumi</h4>
+          <p className={'text-justify'}>Šī sadaļa izklāsta vispārējos jautājumus un principus, kas regulē ceļu satiksmes noteikumus Latvijā. Tā ietver pamata noteikumus, kas jāievēro visiem ceļu satiksmes dalībniekiem, neatkarīgi no transportlīdzekļa veida vai pārvietošanās veida.</p>
+        </div>
+        <div className={'flex flex-col w-full xl:w-3/12  min-h-64 gap-3 relative'}>
+          <Image className={'rounded-md w-full'} src={'https://m-cdn.phonearena.com/images/article/130037-wide-two_1200/How-to-make-your-phone-louder.webp?1612969547'} alt={'chill'} width={'408'} height={'169'}/>
+          <h4 className={'text-left font-bold text-lg'}>1. Vispārīgie jautājumi</h4>
+          <p className={'text-justify'}>Šī sadaļa izklāsta vispārējos jautājumus un principus, kas regulē ceļu satiksmes noteikumus Latvijā. Tā ietver pamata noteikumus, kas jāievēro visiem ceļu satiksmes dalībniekiem, neatkarīgi no transportlīdzekļa veida vai pārvietošanās veida.</p>
+        </div>
+        <div className={'flex flex-col w-full xl:w-3/12  min-h-64 gap-3 relative'}>
+          <Image className={'rounded-md w-full'} src={'https://m-cdn.phonearena.com/images/article/130037-wide-two_1200/How-to-make-your-phone-louder.webp?1612969547'} alt={'chill'} width={'408'} height={'169'}/>
+          <h4 className={'text-left font-bold text-lg'}>1. Vispārīgie jautājumi</h4>
+          <p className={'text-justify'}>Šī sadaļa izklāsta vispārējos jautājumus un principus, kas regulē ceļu satiksmes noteikumus Latvijā. Tā ietver pamata noteikumus, kas jāievēro visiem ceļu satiksmes dalībniekiem, neatkarīgi no transportlīdzekļa veida vai pārvietošanās veida.</p>
+        </div>
+        <div className={'flex flex-col w-full xl:w-3/12  min-h-64 gap-3 relative'}>
+          <Image className={'rounded-md w-full'} src={'https://m-cdn.phonearena.com/images/article/130037-wide-two_1200/How-to-make-your-phone-louder.webp?1612969547'} alt={'chill'} width={'408'} height={'169'}/>
+          <h4 className={'text-left font-bold text-lg'}>1. Vispārīgie jautājumi</h4>
+          <p className={'text-justify'}>Šī sadaļa izklāsta vispārējos jautājumus un principus, kas regulē ceļu satiksmes noteikumus Latvijā. Tā ietver pamata noteikumus, kas jāievēro visiem ceļu satiksmes dalībniekiem, neatkarīgi no transportlīdzekļa veida vai pārvietošanās veida.</p>
+        </div>
+        <div className={'flex flex-col w-full xl:w-3/12  min-h-64 gap-3 relative'}>
+          <Image className={'rounded-md w-full'} src={'https://m-cdn.phonearena.com/images/article/130037-wide-two_1200/How-to-make-your-phone-louder.webp?1612969547'} alt={'chill'} width={'408'} height={'169'}/>
+          <h4 className={'text-left font-bold text-lg'}>1. Vispārīgie jautājumi</h4>
+          <p className={'text-justify'}>Šī sadaļa izklāsta vispārējos jautājumus un principus, kas regulē ceļu satiksmes noteikumus Latvijā. Tā ietver pamata noteikumus, kas jāievēro visiem ceļu satiksmes dalībniekiem, neatkarīgi no transportlīdzekļa veida vai pārvietošanās veida.</p>
+        </div>
+
+      </div>
     </div>
   );
 }
