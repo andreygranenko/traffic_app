a6e8a8f7afc9083e6c078ea2bb7e471962c1c1b8
andreygranenko <131700684+andreygranenko@users.noreply.github.com>
2024-07-02 17:55:47 +0300

    updated modal.jsx

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -8,32 +8,45 @@ const Modal = () => {
       <button onClick={()=>document.getElementById('my_modal_1').showModal()} className="btn btn-active btn-neutral w-44">Gatavoties tūlīt </button>
 
       <dialog id="my_modal_1" style={{width: '400px'}} className="modal mx-auto">
-        <div style={{margin: '0 auto'}} className="modal-box">
+        <div style={{maxHeight: '100vh'}}  className="modal-box overflow-visible">
           <h3 className={'text-lg font-bold'}>Izvēlies tēmu, kuru vēlies studēt</h3>
           <div className="carousel rounded-box  ">
-            <Link href={'/noteikumi'} className="carousel-item w-full relative ">
-              <img
-                src="/frame.png"
-                className="w-full"
-                alt="Tailwind CSS Carousel component" />
-            </Link>
-            <Link href={'/zimes'} className="carousel-item w-full">
-              <img
-                src="/zimes.png"
-                className="w-full"
-                alt="Tailwind CSS Carousel component" />
-            </Link>
-            <Link href={'/testi'} className="carousel-item w-full">
+            <div id={'slide1'} className="carousel-item w-full relative ">
+              <Link href={'/noteikumi'}>
+                <img
+                  src="/frame.png"
+                  className="w-full"
+                  alt="Tailwind CSS Carousel component" />
+              </Link>
+
+            </div>
+            <div id={'slide2'} className="carousel-item w-full relative">
+              <Link href={'/zimes'}>
+                <img
+                  src="/zimes.png"
+                  className="w-full"
+                  alt="Tailwind CSS Carousel component" />
+              </Link>
+
+            </div>
+            <div  id={'slide3'} className="carousel-item w-full relative">
+              <Link href={'/testi'}>
               <img
                 src="/testi.png"
                 className="w-full"
                 alt="Tailwind CSS Carousel component" />
-            </Link>
+              </Link>
+
+            </div>
+          </div>
+          <div className="flex w-full justify-center gap-2 ">
+            <a href="#slide1" className="btn btn-xs">1</a>
+            <a href="#slide2" className="btn btn-xs">2</a>
+            <a href="#slide3" className="btn btn-xs">3</a>
           </div>
           <div className="modal-action">
             <form method="dialog">
-              {/* if there is a button in form, it will close the modal */}
-              <button className="btn">Close</button>
+              <button className="btn">Aizvert</button>
             </form>
           </div>
         </div>
