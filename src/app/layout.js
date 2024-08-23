ec9c7b6f6dc5352301a752cfb0f4f50abb80add5
andreygranenko <131700684+andreygranenko@users.noreply.github.com>
2024-08-23 17:11:36 +0300

    updated page language

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="lv">
       <body className={inter.className + ''}>
       <NavBar/>
       {children}
