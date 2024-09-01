a9bf1f81368e920caf7ef221b2408ed046a85da6
andreygranenko <131700684+andreygranenko@users.noreply.github.com>
2024-09-02 01:28:07 +0300

    added a robots.js

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,8 +1,15 @@
 import {getAllSigns} from "@/app/zimes/page";
+import {fetchAllRules} from "@/lib/fetch/fetch";
 
 export default async function sitemap() {
 
   const allSigns = await getAllSigns();
+
+  const allData = await fetchAllRules();
+
+  const allRulesPaths = allData.map(({number}) => ({
+    url: `${process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'}/noteikumi/${number === 15.1 || number === 22.1 ? number.toString().replace('.', '_') : number}`,
+  }));
 
   const allSignsPaths = allSigns.map(({path}) => ({
     url: `${process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'}/${path}`,
@@ -21,6 +28,7 @@ export default async function sitemap() {
     {
       url: `${process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'}/tests/b-kategorija`,
     },
-    ...allSignsPaths
+    ...allSignsPaths,
+    ...allRulesPaths
     ];
 }
