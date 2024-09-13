02bfb74f90520e4aef87d1e633a6782cde86e996
andreygranenko <131700684+andreygranenko@users.noreply.github.com>
2024-09-13 14:27:42 +0300

    added a road sign test page

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -29,7 +29,7 @@ const NavBar = () => {
               <a>Tests</a>
               <ul className="p-2">
                 <li><Link href={'/tests/b-kategorija'}>B kategorija</Link></li>
-                {/*<li><a>C kategorija</a></li>*/}
+                <li><Link href={'/tests/zimes'}>Ceļa zīmes</Link></li>
               </ul>
             </li>
 
@@ -46,6 +46,7 @@ const NavBar = () => {
               <summary>Tests</summary>
               <ul className="p-2 z-50">
                 <li><Link href={'/tests/b-kategorija'}>B kategorija</Link></li>
+                <li><Link href={'/tests/zimes'}>Ceļa zīmes</Link></li>
                 {/*<li><a>C kategorija</a></li>*/}
               </ul>
             </details>
